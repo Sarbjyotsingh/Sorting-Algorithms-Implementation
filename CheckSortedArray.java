8d0820dbae25b93debcc092951e51f57307580af
Sarbjyotsingh <chahalsarbjyotsingh@gmail.com>
2018-06-29 20:44:56 +0530

    Check Array Sorted!

--- CheckSortedArray.java
+++ CheckSortedArray.java
@@ -1,19 +1,22 @@
 
 public class CheckSortedArray {
-	 public static boolean CheckSorted(int input[], int startIndex) {
-	    	if(startIndex >= input.length-1) {
-	    		return true;
-	    	}
-	    	
-	    	if(input[startIndex] > input[startIndex+1]) {
-				return false;
-				
-			}
-	    	boolean Ans = CheckSorted(input, startIndex+1);
-	    	
-	    	return Ans;
-	    	
-		
-	    }
+	 
+private static boolean CheckSortedMain(int input[], int startIndex) {
+    	if(startIndex >= input.length-1) {
+    		return true;
+    	}
+    	
+    	if(input[startIndex] > input[startIndex+1]) {
+			return false;
+			
+		}
+    	boolean Ans = CheckSortedMain(input, startIndex+1);
+    	
+    	return Ans;
+    	
+	
+    }
+public static boolean CheckSorted(int input[]) {
+	    CheckSortedMain(input,0);
 
 }
